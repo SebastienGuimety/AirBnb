ad591e9e3b5926a507272ccc76405cc53d8cebf3
sebastienguimety <seb.guimety@gmail.com>
2023-03-24 10:30:40 +0100

    Debut Scene Builder

--- src/main/java/com/example/airbnb/HelloApplication.java
+++ src/main/java/com/example/airbnb/HelloApplication.java
@@ -14,6 +14,7 @@ public class HelloApplication extends Application {
         Scene scene = new Scene(fxmlLoader.load(), 320, 240);
         stage.setTitle("Hello!");
         stage.setScene(scene);
+        stage.setMaximized(true);
         stage.show();
     }
 
